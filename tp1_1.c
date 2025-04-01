4f6657e609c9e01fba96880b5ea1b32e364c5557
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-04-01 08:23:50 -0300

    Trabajando con punteros.

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
-    printf("Hola mundo :).");
-    
+    int numero=10;
+    int *puntero;
+    puntero = &numero;
+
+    printf("Contenido del puntero: %d\n",*puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p\n",puntero);
+    printf("Direccion de memoria de la variable: %p\n",&numero);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamanio de memoria utilizada por la variable: %d\n", sizeof(numero));
+
     return 0;
 }
