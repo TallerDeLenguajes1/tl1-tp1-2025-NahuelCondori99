e7aedc77a9887ac2a8a700ffcfef5cfe565decde
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-04-07 22:32:06 -0300

    Realice el apartado b) y realice una correcion en un printf del apartado a)

--- tp1_2.c
+++ tp1_2.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 
 int cuadrado(int num);
+void cuadradoB(int num);
 
 int main()
 {
     int numero, suCuadrado;
 
-    print("Ingresar un numero para calcular su cuadrado:");
+    printf("Ingresar un numero para calcular su cuadrado:");
     scanf("%d",&numero);
 
     suCuadrado = cuadrado(numero);
     printf("El cuadrado de %d es: %d\n", numero, suCuadrado);
+
+    printf("Funcion del apartado b):\n");
+    cuadradoB(numero);
 
     return 0;
 }
@@ -28,4 +32,19 @@ int cuadrado(int num)
     }
     
     return(cuadradoNum);
+}
+
+void cuadradoB(int num)
+{
+    int cuadradoNum = 0;
+
+    if (num!=0)
+    {
+        cuadradoNum = num * num;
+    }else
+    {
+        cuadradoNum = 0;
+    }
+    
+    printf("El cuadrado del numero %d es: %d\n", num, cuadradoNum);
 }
